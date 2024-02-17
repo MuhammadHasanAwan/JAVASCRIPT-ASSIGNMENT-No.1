a70c19f08f43b4644b52d48beb34b006d35ae917
Muhammad Hasan Awan <156433568+MuhammadHasanAwan@users.noreply.github.com>
2024-02-17 20:12:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+// Java script assignment 1
+//Submitted by mirza ahmer baig
+var error= ""
+
+//Quetion # 01
+alert("Hello I AM NEW TO JAVASCRIPT");
+//Quetion # 02
+alert("Your Website is Ranked.");
+//Quetion # 03
+alert("Monitize your website");
+
+//Quetion # 05
+//  <form>
+//         <label>Enter FullName  </label>
+//         <input type="text" name="fullname" placeholder="fullname">
+//         <br>
+//         <label>Enter Email  </label>
+//         <input type="email" name="email" placeholder="email">
+//         <br>
+//         <label>Enter Password  </label>
+//         <input type="password" name="password" placeholder="password">
+//         <br>
+//         <button onclick="document.body.innerHTML += '<p style=\'color: green;\'>Registration completed successfully!</p>';">Submit</button>
+
+//     </form>
+// <script >
+//     alert("please register first to login");
+// </script>
+//Quetion # 07
+var username
+//Quetion # 08
+var myname = "Muhammad Hasan"
+//Quetion # 09
+var message = 'Hello Sir' ;
+console.log(message);
+//Quetion # 10
+var name="Muhammad Hasan"
+var Age =17
+var Address="Mehmoodabad No.2"
+alert("Name: " + name);
+alert("Age: " + Age);
+alert("Address: " + Address);
+//Quetion # 11
+var email ="www.muhammadhasan2005@gmail.com";
+alert("Email : "+ email)
+//Quetion # 12,13
+var string1=":)";
+alert(string1);
+console.log(string1);
+//Quetion # 14
+var Age =20
+alert("Age: " + Age);
+//Quetion # 15
+var N=1;
+N++;
+var visits ="you visited this website "+N+"times";
+alert(visits);
+//Quetion # 16
+var  birthYear=2005;
+alert(birthYear)
+//Quetion # 17
+var visitorName = "Customer";
+var productTitle = "Pants";
+var quantity = 5;
+alert(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+//Quetion # 18
+var variable1 = "aa", variable2 = 12, variable3 = true;
+console.log(variable1+variable2+variable3);
+//Quetion # 19
+//legal variables
+var myVariable
+var _value
+var $price
+var firstName
+var totalCount
+ //Illegal Variable Names:
+// var 1stPlace 
+// var my-variable
+// var illegal variable 
+// var my.variable 
+// var break 
+//Quetion # 20
+alert("Rules for naming JS variables\n\n" +
+      "Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable.\n\n" +
+      "Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name.\n\n" +
+      "Variable names are case-sensitive.\n\n" +
+      "Variable names should not be JavaScript keywords.");
+
+//Quetion # 21
+var number1 = 10;
+var number2 = 20;
+var sum = number1 + number2;
+console.log("The sum of " + number1 + " and " + number2 + " is: " + sum);
+//Quetion # 22
+var number1 = 10;
+var number2 = 20;
+var mult=number1*number2
+console.log("The multiplication of " + number1 + " and " + number2 + " is: " + mult);
+var sub=number1-number2
+console.log("The subtraction of " + number1 + " and " + number2 + " is: " + sub);
+var divs=number1/number2
+console.log("The division of " + number1 + " and " + number2 + " is: " + divs);
+//Quetion # 23
+// a. Declare a variable.
+var myVariable;
+
+// b. Show the value of the variable after declaration.
+console.log("Value after variable declaration is: " + myVariable);
+
+// c. Initialize the variable with some number.
+myVariable = 5;
+
+// d. Show the initial value of the variable.
+console.log("Initial value: " + myVariable);
+
+// e. Increment the variable.
+myVariable++;
+
+// f. Show the value of the variable after increment.
+console.log("Value after increment is: " + myVariable);
+
+// g. Add 7 to the variable.
+myVariable += 7;
+
+// h. Show the value of the variable after addition.
+console.log("Value after addition is: " + myVariable);
+
+// i. Decrement the variable.
+myVariable--;
+
+// j. Show the value of the variable after decrement.
+console.log("Value after decrement is: " + myVariable);
+
+// k. Show the remainder after dividing the variable's value by 3.
+var remainder = myVariable % 3;
+
+// l. Output: "The remainder is: 0"
+console.log("The remainder is: " + remainder);
+
+//Quetion # 24
+var ticketPrice = 600;
+var numberOfTickets = 5;
+var totalCost = ticketPrice * numberOfTickets;
+console.log("The cost of buying " + numberOfTickets + " movie tickets is: " + totalCost );
